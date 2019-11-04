69559c52f391b608d1aa0bded5153123c6aa6723
mustaevtt <company.abb@gmail.com>
2019-11-04 13:13:44 +0300

    #fixed

--- src/test/java/part1/lesson03/task03/MyHashMapTest.java
+++ src/test/java/part1/lesson03/task03/MyHashMapTest.java
@@ -1,20 +1,12 @@
 package part1.lesson03.task03;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class MyHashMapTest {
 
-    @BeforeEach
-    void setUp() {
-    }
 
-    @AfterEach
-    void tearDown() {
-    }
 
     @Test
     void put() {
@@ -45,7 +37,5 @@ class MyHashMapTest {
         assertEquals(2,myHashMap.getSize());
     }
 
-    @Test
-    void getSize() {
-    }
+
 }
